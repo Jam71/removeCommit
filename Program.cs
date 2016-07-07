808fef0c47ec80996d26ee5c16d3024a94a24784
Jamie <jamwiong@gmail.com>
2016-07-07 13:53:20 -0600

    Add new Branch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 2. Add new commits. 
-
+9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.9. Add new file on new branch.
 1. Use rebase first.
 4. another commit.
 5. Add command;
